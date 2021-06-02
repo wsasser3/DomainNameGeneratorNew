ddec8446f6dde6a96782aa096bd6b673f6fd08a9
wsasser3 <wsasser72@gmail.com>
2021-06-02 00:01:29 +0000

    Domain Name Assignment

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainNameGen() {
+  let pronounArray = ["my", "his", "your"];
+  let adjectiveArray = ["hot", "dirty", "sleeping"];
+  let nounArray = ["brain", "car", "puppy"];
+  let suffixArray = [".com", ".org", ".net"];
+  let domainName = "";
+  for (let i = 0; i < pronounArray.length; i++) {
+    for (let j = 0; j < adjectiveArray.length; j++) {
+      for (let k = 0; k < nounArray.length; k++) {
+        for (let l = 0; l < suffixArray.length; l++) {
+          domainName +=
+            pronounArray[i] + adjectiveArray[j] + nounArray[k] + suffixArray[l];
+        }
+      }
+    }
+  }
+  return domainName;
+}
+
+console.log(domainNameGen());
+//domainNameGen();
